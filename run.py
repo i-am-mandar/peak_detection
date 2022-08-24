6d1f04dd4716a32ea0b6158c9adb9c5e622a04a1
Md Rizwanul Islam <riad.rizwanul@gmail.com>
2022-08-24 13:09:58 +0600

    Added by Rizwan for getting 2 sine wave, normalized it and used fftfreq to plot

--- run.py
+++ run.py
@@ -39,6 +39,42 @@ plt.plot(fftx, np.abs(ffty[0]))
 plt.show()
 
 
+# Added by Rizwan for getting 2 sine wave, normalized it and used fftfreq to plot
+# import numpy as np 
+# import scipy, matplotlib
+# from matplotlib import pyplot as plt
+# from scipy.fft import fft, fftfreq
+
+# SAMPLE_RATE = 44100  # Hertz
+# DURATION = 5  # Seconds
+
+
+
+# def generate_sine_wave(freq, sample_rate, duration):
+    # x = np.linspace(0, duration, sample_rate * duration, endpoint=False)
+    # frequencies = x * freq
+    # # 2pi because np.sin takes radians
+    # y = np.sin((2 * np.pi) * frequencies)
+    # return x, y
+
+# # Generate a 2 hertz sine wave that lasts for 5 seconds
+# #x, y = generate_sine_wave(2, SAMPLE_RATE, DURATION)
+
+# _, nice_tone = generate_sine_wave(400, SAMPLE_RATE, DURATION)
+# _, noise_tone = generate_sine_wave(4000, SAMPLE_RATE, DURATION)
+# noise_tone = noise_tone * 0.3
+
+# mixed_tone = nice_tone + noise_tone
+# normalized_tone = np.int16((mixed_tone / mixed_tone.max()) * 32767)
+
+# N = SAMPLE_RATE * DURATION
+
+# yf = fft(normalized_tone)
+# xf = fftfreq(N, 1 / SAMPLE_RATE)
+
+# plt.plot(xf, np.abs(yf))
+# plt.show()
+# end
 
 # 
 # Generic Approach
