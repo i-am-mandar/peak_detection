8eadfc86f29e6d65ee55f0c2ec90f234efc04319
Mandar Patkar <mandar.patkar@stud.fra-uas.de>
2022-09-04 12:31:57 +0200

    code cleaning and implemented window on fft

--- run.py
+++ run.py
@@ -1,86 +1,85 @@
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-
-dataset = "dataset\T_File_5.xlsx"
-df = pd.read_excel(dataset)
-
-df = df.iloc[0: ,6:]
-#print(df)
-
-#length of column
-col_len = len(df.columns)
+#import numpy as np
+#import pandas as pd
+#import matplotlib.pyplot as plt
+#from scipy.fft import fft, fftfreq
+#
+#dataset = "dataset\T_File_5.xlsx"
+#df = pd.read_excel(dataset)
+#
+#df = df.iloc[0: ,6:]
+##print(df)
+#
+##length of column
+#col_len = len(df.columns)
 #print(col_len)
+#
+##length of row
+#row_len = len(df.index)
+##print(row_len)
+#
+##first row of df
+#row_1 = df.head(1)
+##425 * 8 = 3400
+#step = 425
+#for i in range(step, col_len+1,step):
+#    y = row_1.iloc[: ,(i-step) : i]
+#    n = y.size
+#    timestep = 0.01
 
-#length of column
-row_len = len(df.index)
-#print(row_len)
+#    ffty = np.fft.rfft(y)
 
-#first row of df
-row_1 = df.head(1)
+#    fftx = np.fft.rfftfreq(n, d=timestep)
+#    print(len(ffty[0]))
+#    print(len(fftx))
 
-y = row_1.iloc[: ,128 : 256]
-print(y)
-n = 128
-print(y)
+#    plt.grid()
+#    plt.plot(fftx, np.abs(ffty[0]))
+#    plt.show()
 
-ffty = np.fft.rfft(y)
-#fftx = np.fft.fftfreq(n, 1/5000)[:n//2]
-fftx = np.fft.rfftfreq(n, 1/5000)
-#default sample space (inverse of sampling rate) is 1
 
-print(ffty[0])
-print(fftx)
+#Added by Rizwan for getting 2 sine wave, normalized it and used fftfreq to plot
+import numpy as np 
+import scipy, matplotlib
+from matplotlib import pyplot as plt
+from scipy.fft import fft, fftfreq
 
-plt.grid()
-#abs_ffty = 2.0/n * np.abs(ffty[0:n//2])
-#plt.plot(fftx, abs_ffty[0])
-plt.plot(fftx, np.abs(ffty[0]))
+SAMPLE_RATE = 44100  # Hertz
+DURATION = 5  # Seconds
+
+
+
+def generate_sine_wave(freq, sample_rate, duration):
+  x = np.linspace(0, duration, sample_rate * duration, endpoint=False)
+  frequencies = x * freq
+  # 2pi because np.sin takes radians
+  y = np.sin((2 * np.pi) * frequencies)
+  return x, y
+
+# Generate a 2 hertz sine wave that lasts for 5 seconds
+x, y = generate_sine_wave(2, SAMPLE_RATE, DURATION)
+
+_, nice_tone = generate_sine_wave(400, SAMPLE_RATE, DURATION)
+_, noise_tone = generate_sine_wave(4000, SAMPLE_RATE, DURATION)
+noise_tone = noise_tone * 0.3
+
+mixed_tone = nice_tone + noise_tone
+normalized_tone = np.int16((mixed_tone / mixed_tone.max()) * 32767)
+
+N = SAMPLE_RATE * DURATION
+
+yf = fft(normalized_tone)
+print(yf[395:405])
+xf = fftfreq(N, 1 / SAMPLE_RATE)
+#The maximum frequency is half the sample rate
+points_per_freq = len(xf) / (SAMPLE_RATE / 2)
+
+# Our target frequency is 4000 Hz
+target_idx = int(points_per_freq * 4000)
+
+yf[target_idx - 1 : target_idx + 2] = 0
+plt.plot(xf, np.abs(yf))
 plt.show()
-
-
-# Added by Rizwan for getting 2 sine wave, normalized it and used fftfreq to plot
-# import numpy as np 
-# import scipy, matplotlib
-# from matplotlib import pyplot as plt
-# from scipy.fft import fft, fftfreq
-
-# SAMPLE_RATE = 44100  # Hertz
-# DURATION = 5  # Seconds
-
-
-
-# def generate_sine_wave(freq, sample_rate, duration):
-    # x = np.linspace(0, duration, sample_rate * duration, endpoint=False)
-    # frequencies = x * freq
-    # # 2pi because np.sin takes radians
-    # y = np.sin((2 * np.pi) * frequencies)
-    # return x, y
-
-# # Generate a 2 hertz sine wave that lasts for 5 seconds
-# #x, y = generate_sine_wave(2, SAMPLE_RATE, DURATION)
-
-# _, nice_tone = generate_sine_wave(400, SAMPLE_RATE, DURATION)
-# _, noise_tone = generate_sine_wave(4000, SAMPLE_RATE, DURATION)
-# noise_tone = noise_tone * 0.3
-
-# mixed_tone = nice_tone + noise_tone
-# normalized_tone = np.int16((mixed_tone / mixed_tone.max()) * 32767)
-
-# N = SAMPLE_RATE * DURATION
-
-# yf = fft(normalized_tone)
-# xf = fftfreq(N, 1 / SAMPLE_RATE)
-# The maximum frequency is half the sample rate
-# points_per_freq = len(xf) / (SAMPLE_RATE / 2)
-
-# # Our target frequency is 4000 Hz
-# target_idx = int(points_per_freq * 4000)
-
-# yf[target_idx - 1 : target_idx + 2] = 0
-# plt.plot(xf, np.abs(yf))
-# plt.show()
-# end
+end
 
 # 
 # Generic Approach
